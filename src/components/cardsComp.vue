f516aa16ae4eb619c2c9e9862b12dfce0bc36bfd
Pirisinu <palazzialessandro2@gmail.com>
2023-10-12 16:59:15 +0200

    [ADD]API

--- src/components/cardsComp.vue
+++ src/components/cardsComp.vue
@@ -1,20 +1,32 @@
 <script>
+import axios from 'axios';
 import { store } from '../data/store.js'
 export default {
   name: 'cardsComp',
   data(){
     return{
-      
+      apiLimit: '?num=20&offset=0'
+    }
+  },
+  methods:{
+    getApi(){
+      axios.get(store.apiUrl+this.apiLimit)
+        .then( (card) => {
+          store.arrayCards.push(card.data)
+
+        } )
     }
   },
   mounted(){
-    console.log(store.apiUrl);
+    this.getApi()
+  console.log(store.apiUrl+this.apiLimit);
+  console.log(store.arrayCards);
   }
 }
 </script>
 
 <template>
-  <p>Cards</p>
+  <div></div>
 </template>
 
 <style lang="scss" scoped>
